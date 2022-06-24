cfcc179863e54b25794f11e185497448235ddc0d
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-06-24 11:09:50 +0530

    added chart in home page using recharts library

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Chart from '../../components/featuredInfo/chart/Chart'
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
+import { userData } from '../../components/featuredInfo/chart/dummyData'
 import './Home.css'
+
 export default function Home() {
+
     return (
         <div className='home'>
             <FeaturedInfo />
+            <Chart data={userData} title='User Analytics' grid dataKey='Active User' />
         </div>
     )
 }
