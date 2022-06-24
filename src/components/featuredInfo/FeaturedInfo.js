5fed2c1e2795a233a80efc28c31e3c1458d83355
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-06-24 10:42:50 +0530

    featured info cards modified with styles

--- src/components/featuredInfo/FeaturedInfo.js
+++ src/components/featuredInfo/FeaturedInfo.js
@@ -8,24 +8,24 @@ export default function FeaturedInfo() {
             <div className='featuredItem'>
                 <span className='featuredTitle'>Revanue</span>
                 <div className='featuredMoneyContainer'>
-                    <span className='featuredMoney'>$3</span>
-                    <span className='featuredMoneyRate'>-11.4<ArrowDownward /></span>
+                    <span className='featuredMoney'>$2,415</span>
+                    <span className='featuredMoneyRate'>-11.4<ArrowDownward className='featuredIcon negative' /></span>
                 </div>
                 <span className='featuredSub'>Compared to last month</span>
             </div>
             <div className='featuredItem'>
                 <span className='featuredTitle'>Sales</span>
                 <div className='featuredMoneyContainer'>
-                    <span className='featuredMoney'>$3,215</span>
-                    <span className='featuredMoneyRate'>-1.4<ArrowDownward /></span>
+                    <span className='featuredMoney'>$4,215</span>
+                    <span className='featuredMoneyRate'>-1.4<ArrowDownward className='featuredIcon negative' /></span>
                 </div>
                 <span className='featuredSub'>Compared to last month</span>
             </div>
             <div className='featuredItem'>
                 <span className='featuredTitle'>Cost</span>
                 <div className='featuredMoneyContainer'>
-                    <span className='featuredMoney'>$3,5412</span>
-                    <span className='featuredMoneyRate'>+4.2<ArrowUpward /></span>
+                    <span className='featuredMoney'>$3,412</span>
+                    <span className='featuredMoneyRate'>+4.2<ArrowUpward className='featuredIcon' /></span>
                 </div>
                 <span className='featuredSub'>Compared to last month</span>
             </div>
