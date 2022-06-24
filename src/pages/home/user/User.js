fefbab3909188632ccc03af5163b48b09496ab50
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-06-24 18:07:10 +0530

    show profile widget completed with including css, working on edit profile widget

--- src/pages/home/user/User.js
+++ src/pages/home/user/User.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './User.css'
+import { PermIdentity, CalendarToday, PhoneAndroid, MailOutline, LocationSearching } from '@material-ui/icons'
 
 export default function User() {
     return (
@@ -7,6 +8,42 @@ export default function User() {
             <div className='userTitleContainer'>
                 <h1 className='userTitle'>Edit User</h1>
                 <button className='userAddButton'>Create</button>
+            </div>
+            <div className='userContainer'>
+                <div className='userShow'>
+                    <div className='userShowTop'>
+                        <img className='userShowImg' alt='pic' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnS1o3mO3S_Nkfw1WAGaRJ6KaOGgODpfoOsA&usqp=CAU'></img>
+                        <div className='userShowTopTitle'>
+                            <span className='userShowUsername'>Anna Beck</span>
+                            <span className='userShowUserTitle'>Software Engineer</span>
+                        </div>
+                    </div>
+                    <div className='userShowBottom'>
+                        <span className='userShowTitle'>Account Details</span>
+                        <div className='userShowInfo'>
+                            <PermIdentity className='userShowIcon' />
+                            <span className='userShowInfoTitle'>annabeck99</span>
+                        </div>
+                        <div className='userShowInfo'>
+                            <CalendarToday className='userShowIcon' />
+                            <span className='userShowInfoTitle'>10-08-2000</span>
+                        </div>
+                        <span className='userShowTitle'>Contact Details</span>
+                        <div className='userShowInfo'>
+                            <PhoneAndroid className='userShowIcon' />
+                            <span className='userShowInfoTitle'>+1 234 524 52</span>
+                        </div>
+                        <div className='userShowInfo'>
+                            <MailOutline className='userShowIcon' />
+                            <span className='userShowInfoTitle'>anna@email.com</span>
+                        </div>
+                        <div className='userShowInfo'>
+                            <LocationSearching className='userShowIcon' />
+                            <span className='userShowInfoTitle'>New York | USA</span>
+                        </div>
+                    </div>
+                </div>
+                <div className='userUpdate'></div>
             </div>
         </div>
     )
