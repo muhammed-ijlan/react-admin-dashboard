7066f5dcad24bf07edef126072b636ce61c8af97
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-06-29 17:27:46 +0530

    updated code moved from app js to home js, Completed chart data fetching

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import axios from 'axios'
+
 
 import "./App.css";
 import UserList from "./pages/userlist/UserList";
@@ -12,36 +12,9 @@ import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
-import { useEffect, useState } from "react";
+
 
 function App() {
-  const [userStats, setUserStats] = useState([])
-
-  const MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Oct", "Nov", "Dec"];
-
-  useEffect(() => {
-    const getStats = async () => {
-      try {
-        const res = await axios.get("/users/stats", {
-          headers: {
-            token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYjk4NWY5YTg5ODQ0YjUzOTk5MjQwYSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NjQ4NDcyNywiZXhwIjoxNjU2OTE2NzI3fQ.sJtjhrJBpqZV-KrWiSU-WELuk4KjsW-wFdWFaxfoVw8"
-          }
-        })
-
-        setUserStats(res.data)
-
-      } catch (err) {
-        console.log(err);
-      }
-    }
-
-
-
-
-    getStats();
-  }, [])
-
-  console.log(userStats);
 
   return (
     <Router>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useEffect, useMemo, useState } from "react"; import axios from 'axios'
 import Chart from '../../components/featuredInfo/chart/Chart'
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import { userData } from '../../components/featuredInfo/chart/dummyData'
@@ -9,10 +9,38 @@ import './Home.css'
 
 export default function Home() {
 
+    const [userStats, setUserStats] = useState([])
+
+    const MONTHS = useMemo(() => ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Oct", "Nov", "Dec"], [])
+
+    useEffect(() => {
+        const getStats = async () => {
+            try {
+                const res = await axios.get("/users/stats", {
+                    headers: {
+                        token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYjk4NWY5YTg5ODQ0YjUzOTk5MjQwYSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NjQ4NDcyNywiZXhwIjoxNjU2OTE2NzI3fQ.sJtjhrJBpqZV-KrWiSU-WELuk4KjsW-wFdWFaxfoVw8"
+                    }
+                })
+
+                const statsList = res.data.sort(function (a, b) {
+                    return a._id - b._id
+                })
+
+                statsList.map(item => setUserStats(prev => [...prev, { name: MONTHS[item._id - 1], "New User": item.total }]))
+
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        getStats();
+    }, [MONTHS])
+
+
+
     return (
         <div className='home'>
             <FeaturedInfo />
-            <Chart data={userData} title='User Analytics' grid dataKey='Active User' />
+            <Chart data={userStats} title='User Analytics' grid dataKey='New User' />
             <div className='homeWidgets'>
                 <WidgetSm />
                 <WidgetLg />
