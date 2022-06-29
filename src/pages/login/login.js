2a00201bd34cc162c6516b1f421935a372704d0e
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-06-29 23:29:00 +0530

    added completed login page functionality, added api calls js file

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,13 +1,17 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './login.css'
+import { login } from '../../context/authContext/apiCalls'
+import { AuthContext } from '../../context/authContext/AuthContext'
+
 
 export default function () {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const { isFetching, dispatch } = useContext(AuthContext)
 
     function handleLogin(e) {
         e.preventDefault();
-
+        login({ email, password }, dispatch)
 
     }
 
@@ -16,7 +20,7 @@ export default function () {
             <form className="loginForm">
                 <input placeholder='email' type="text" className="loginInput" onChange={(e) => setEmail(e.target.value)} />
                 <input placeholder='Password' type="password" className="loginInput" onChange={(e) => setPassword(e.target.value)} />
-                <button className="loginButton" onClick={handleLogin}>Login</button>
+                <button className="loginButton" onClick={handleLogin} disabled={isFetching}>Login</button>
 
             </form>
 
