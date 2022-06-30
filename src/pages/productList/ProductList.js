7cd04a3085b255cb1142da25333bc4aa6fd19679
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-06-30 16:25:35 +0530

    added data to our movies table

--- src/pages/productList/ProductList.js
+++ src/pages/productList/ProductList.js
@@ -1,23 +1,28 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import { DataGrid } from "@material-ui/data-grid";
 import { DeleteOutline } from "@material-ui/icons";
 import { productRows } from "../../components/featuredInfo/chart/dummyData";
 import { Link } from "react-router-dom";
 import "./ProductList.css";
+import { MovieContext } from "../../context/movieContext/MovieContext"
+import { getMovies } from '../../context/movieContext/apiCalls'
 
 export default function ProductList() {
-    const [data, setData] = useState(productRows);
+    const { movies, dispatch } = useContext(MovieContext)
+
+    useEffect(() => {
+        getMovies(dispatch)
+    }, [dispatch])
 
     const handleDelete = (id) => {
-        setData(data.filter((item) => item.id !== id));
+        // setData(data.filter((item) => item.id !== id));
     };
-
     const columns = [
-        { field: "id", headerName: "ID", width: 90 },
+        { field: "_id", headerName: "ID", width: 90 },
         {
-            field: "product",
-            headerName: "Product",
+            field: "movie",
+            headerName: "Movie",
             width: 200,
             renderCell: (params) => {
                 return (
@@ -27,23 +32,15 @@ export default function ProductList() {
                             src={params.row.image}
                             alt="pic"
                         ></img>
-                        {params.row.name}
+                        {params.row.title}
                     </div>
                 );
             },
         },
-        { field: "stock", headerName: "Stock", width: 200 },
-        {
-            field: "status",
-            headerName: "Status",
-            type: "number",
-            width: 120,
-        },
-        {
-            field: "price",
-            headerName: "Price",
-            width: 160,
-        },
+        { field: "genre", headerName: "Genre", width: 120 },
+        { field: "year", headerName: "Year", width: 120 },
+        { field: "limit", headerName: "limit", width: 120 },
+        { field: "isSeries", headerName: "Is Series", width: 120 },
         {
             field: "action",
             headerName: "Action",
@@ -67,12 +64,13 @@ export default function ProductList() {
     return (
         <div className="productList">
             <DataGrid
-                rows={data}
+                rows={movies}
                 disableSelectionOnClick
                 columns={columns}
                 pageSize={8}
                 rowsPerPageOptions={[5]}
                 checkboxSelection
+                getRowId={r => r._id}
             />
         </div>
     );
