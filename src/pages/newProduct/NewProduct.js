0df32910c319aa07bf8c14343905e44da7e60896
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-07-01 17:06:19 +0530

    updated our new Movies form and added style

--- src/pages/newProduct/NewProduct.js
+++ src/pages/newProduct/NewProduct.js
@@ -3,26 +3,58 @@ import "./NewProduct.css";
 export default function NewProduct() {
     return (
         <div className="newProduct">
-            <h1 className="addProductTitle">New Product</h1>
+            <h1 className="addProductTitle">New Movie</h1>
             <form className="addProductForm">
                 <div className="addProductItem">
                     <label>Image</label>
-                    <input type="file" id="file" />
+                    <input type="file" id="img" />
                 </div>
                 <div className="addProductItem">
-                    <label>Name</label>
-                    <input type="text" placeholder="Apple Airpods" />
+                    <label>Title Image</label>
+                    <input type="file" id="imgTitle" />
                 </div>
                 <div className="addProductItem">
-                    <label>Stock</label>
-                    <input type="text" placeholder="123" />
+                    <label>Thumbnail Image</label>
+                    <input type="file" id="imgSmall" />
                 </div>
                 <div className="addProductItem">
-                    <label>Active</label>
-                    <select name="active" id="active">
-                        <option value="yes">Yes</option>
-                        <option value="no">No</option>
+                    <label>Title</label>
+                    <input type="text" placeholder="John wick" />
+                </div>
+                <div className="addProductItem">
+                    <label>Description</label>
+                    <input type="text" placeholder="description" />
+                </div>
+                <div className="addProductItem">
+                    <label>Year</label>
+                    <input type="text" placeholder="year" />
+                </div>
+                <div className="addProductItem">
+                    <label>Genre</label>
+                    <input type="text" placeholder="description" />
+                </div>
+                <div className="addProductItem">
+                    <label>Limit</label>
+                    <input type="text" placeholder="description" />
+                </div>
+                <div className="addProductItem">
+                    <label>Duration</label>
+                    <input type="text" placeholder="description" />
+                </div>
+                <div className="addProductItem">
+                    <label>Is Series?</label>
+                    <select name="active" id="isSeries">
+                        <option value="false">No</option>
+                        <option value="true">Yes</option>
                     </select>
+                </div>
+                <div className="addProductItem">
+                    <label>Trailer</label>
+                    <input type="file" />
+                </div>
+                <div className="addProductItem">
+                    <label>Video</label>
+                    <input type="file" />
                 </div>
                 <button className="addProductButton">Create</button>
             </form>
